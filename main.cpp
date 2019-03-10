815a2cdcdb5754ea5e546963e90ee522c08d5e58
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2019-03-10 03:22:44 +0000

    multirun version

--- main.cpp
+++ main.cpp
@@ -5,13 +5,19 @@
 #include "algorithm.hpp"
 #include <boost/gil/image.hpp>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "usage: program filter-application-count\n";
+    }
     gil::rgb8_image_t image;
     shino::read_image("input.png", image);
-    auto output_image = gil::rgb8_image_t(image);
+    auto repetition_count = std::stoul(argv[1]);
+    for (std::size_t i = 0; i < repetition_count; ++i) {
+        auto output_image = gil::rgb8_image_t(image);
+        shino::apply_gaussian_blur(image, output_image);
+        image = output_image;
+    }
 
-    shino::apply_gaussian_blur(image, output_image);
-
-    shino::write_image("output.png", output_image);
+    shino::write_image("output.png", image);
     // shino::write_image("output.ong", image);
 }
